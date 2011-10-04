3532ea98987ebd96c71e9bf930ca427824ca88c2
Cezar Sincan <cezarsincan@gmail.com>
2011-10-05 01:51:51 +0200

    helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  #Return titlte on a per-page basis.
+
+  def title
+    base_title  = "Sample_app"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+
+  end
 end
